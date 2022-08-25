4bc5eae3603b722f1cccb259fb430eeb67e8650b
Angel <angel_la12@hotmail.com.ar>
2022-08-25 21:52:26 +0200

    fix pulsador

--- main.ino
+++ main.ino
@@ -1,3 +1,40 @@
+ class Pulsador {
+  private:
+    int pin;
+    bool flanco = HIGH;
+    bool estado_anterior = !flanco;
+
+    //metodo
+  public:
+    Pulsador(int p) {
+      pin = p;
+
+      pinMode(pin, INPUT);
+    }
+
+    void setFlanco(bool f) {
+      flanco = f;
+      estado_anterior = !flanco;
+    }
+
+    //metodos o acciones
+    bool getIsPress() {
+      bool estado_actual = digitalRead(pin);
+      bool estado = (estado_anterior != estado_actual) && estado_actual == flanco;
+      estado_anterior = estado_actual;
+      return estado;
+    }
+
+    String getIsPressText() {
+      if (getIsPress()) return "PRESS";
+      else return "IS NOT PRESS";
+    }
+
+};
+
+
+
+
 //motor
 
 #define MOTOR_IS 0
@@ -52,7 +89,8 @@ public:
     if(velocidad<0)velocidad=0;
   }
 };
-
+#define PIN_PULSADOR_1 3
+Pulsador *p1 = new Pulsador(PIN_PULSADOR_1);
 Motor *motor = new Motor(MOTOR_IS, MOTOR_EN, MOTOR_PWM);
 
 
